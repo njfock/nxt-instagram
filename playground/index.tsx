8f76c68af003880b8a7da165d2d5b0ffd3c15321
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-07-26 16:13:09 -0600

    test interface method typescript

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,27 +1,11 @@
 interface IPrueba {
     texto: string
-    numero?: number
-    booleano?: boolean
-    obj?:{
-        texto: string
-        numero: number
-        booleano: boolean
-    }
-    arr?: string[]
-    arrN?: number[]
-    arrB?: boolean[]
+    met(x: string): number
 }
-
-interface IUser{
-    name: string
-}
-
-const fn = (x: IPrueba): IUser[] => {
-   return [{name: ''}]
-}
-
-fn({texto:''})
 
 class ClassPrueba implements IPrueba {
-
+    public texto = 'Texto!'
+    public met(x: string){
+        return 1
+    }
 }
