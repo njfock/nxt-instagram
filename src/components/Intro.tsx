e9c21b520d187b0eab31ab3dbf34a1f3c7ac6de3
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-07-26 15:29:32 -0600

    create component intro

---
+++ src/components/Intro.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Intro extends React.Component{
+    public render(){
+        return(
+            <div className="App-intro">
+                <span>Nexthor</span>
+            </div>
+        )
+    }
+}
