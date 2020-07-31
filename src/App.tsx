bbef9eb28b41e47ae5144b807cff6eea9d4625f8
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-07-30 18:59:15 -0600

    reiniciando

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Intro />
-      <Intro text='Nexthor'/>
+      
     </div>
   );
 }
