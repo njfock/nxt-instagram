46941746e9968da1e535d82ca2506b9832b6988c
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-07-30 20:05:28 -0600

    create component card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Card>lALA</Card>
   );
 }
 
