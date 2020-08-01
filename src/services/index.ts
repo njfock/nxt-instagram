b266bb15507e5e72290833fa0514357acfca9436
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-08-01 08:56:10 -0600

    thunk for login

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,14 @@
+import { firestore, storage, auth} from 'firebase'
 import * as firebase from "./firebase"
 
 const services = {
     ...firebase,
+}
+
+export interface IServices {
+    db: firestore.Firestore
+    storage: storage.Storage
+    auth: auth.Auth
 }
 
 export default services
