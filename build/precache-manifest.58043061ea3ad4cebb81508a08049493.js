557a4dd52247808ec88a62f938bd3586af8b20e3
Nestor Fock <njfock@nuestrodiario.com.gt>
2020-08-01 20:36:44 -0600

    first build

---
+++ build/precache-manifest.58043061ea3ad4cebb81508a08049493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce0f87d6b7db8c69e62c08695c2c55a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b6955c0c52440353989",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9eaa3224e4956f696703",
+    "url": "/static/js/2.47bd4873.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.47bd4873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6955c0c52440353989",
+    "url": "/static/js/main.ea7404c5.chunk.js"
+  },
+  {
+    "revision": "78aa933dc0bdaba1062b",
+    "url": "/static/js/runtime-main.c2325826.js"
+  }
+]);
